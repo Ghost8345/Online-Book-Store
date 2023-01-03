92c2eeff4a7f70f787f03d005d4f1700346aca57
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2023-01-03 17:04:42 +0200

    :zap: Added edit book + apis

--- frontend/src/app/components/add-book/add-book.component.ts
+++ frontend/src/app/components/add-book/add-book.component.ts
@@ -18,10 +18,11 @@ export class AddBookComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
 
 
-  uploadItem = new UploadItem('','', '','', '','', 0, 0, 0, 0);
-  categories = ['Science', 'Art', 'Religion', 'History Geography'];
+  uploadItem = new UploadItem(0,'', 0,'', '','', 0, 0, 0, '');
+  categories = ['Science', 'Art', 'Religion', 'History', 'Geography'];
   categoryName: string = "";
-
+  publishers = [];
+  publihserName: string = "";
   imageSrc: string = "";
   imageName: string = "";
   imageBlob: string = "";
@@ -50,7 +51,7 @@ export class AddBookComponent implements OnInit {
         this.imageName = file.name
         this.imageBlob = file
         this.imageSrc = reader.result as string;
-        this.uploadItem.cover = this.imageSrc
+        this.uploadItem.coverImage = this.imageSrc
         console.log(reader.result)
 
         this.myForm.patchValue({ fileSource: reader.result as string });
@@ -67,67 +68,61 @@ export class AddBookComponent implements OnInit {
 
   onSubmit() {
     console.log(' title: ' + this.uploadItem.title + ', description: ' + 'price: ' + this.uploadItem.price + 'category ');
-//this.upload
+    this.uploadItem.category=this.categoryName;
+    this.upload();
   }
   upload(){
-    // this.uploadItem.category_id = this.categories.indexOf(this.categoryName)
-    if (this.uploadItem.title.length < 3) {
-      // Swal.fire({
-      //   position: 'center',
-      //   icon: 'warning',
-      //   title: 'Title must be at least 3 characters',
-      //   showConfirmButton: false,
-      //   timer: 2000
-      // })
-      return;
-    }
-   
-    if (this.uploadItem.price == 0) {
-      // Swal.fire({
-      //   position: 'center',
-      //   icon: 'warning',
-      //   title: 'Price can not be zero',
-      //   showConfirmButton: false,
-      //   timer: 2000
-      // })
-      return;
-    }
-    // if (this.uploadItem.category == -1) {
-    //   Swal.fire({
-    //     position: 'center',
-    //     icon: 'warning',
-    //     title: 'You must choose a category',
-    //     showConfirmButton: false,
-    //     timer: 2000
-    //   })
+    // // this.uploadItem.category_id = this.categories.indexOf(this.categoryName)
+    // if (this.uploadItem.title.length < 3) {
+    //   // Swal.fire({
+    //   //   position: 'center',
+    //   //   icon: 'warning',
+    //   //   title: 'Title must be at least 3 characters',
+    //   //   showConfirmButton: false,
+    //   //   timer: 2000
+    //   // })
     //   return;
     // }
-    if (this.uploadItem.cover == "") {
-      // Swal.fire({
-      //   position: 'center',
-      //   icon: 'warning',
-      //   title: 'You must provide a photo for your product',
-      //   showConfirmButton: false,
-      //   timer: 2000
-      // })
-      return;
-    }
+   
+    // if (this.uploadItem.price == 0) {
+    //   // Swal.fire({
+    //   //   position: 'center',
+    //   //   icon: 'warning',
+    //   //   title: 'Price can not be zero',
+    //   //   showConfirmButton: false,
+    //   //   timer: 2000
+    //   // })
+    //   return;
+    // }
+    // // if (this.uploadItem.category == -1) {
+    // //   Swal.fire({
+    // //     position: 'center',
+    // //     icon: 'warning',
+    // //     title: 'You must choose a category',
+    // //     showConfirmButton: false,
+    // //     timer: 2000
+    // //   })
+    // //   return;
+    // // }
+    // if (this.uploadItem.cover == "") {
+    //   // Swal.fire({
+    //   //   position: 'center',
+    //   //   icon: 'warning',
+    //   //   title: 'You must provide a photo for your product',
+    //   //   showConfirmButton: false,
+    //   //   timer: 2000
+    //   // })
+    //   return;
+    // }
 
 
 
     const headerr = new HttpHeaders({ 'Content-Type': 'application/json' });
-    this.http.post('http://localhost:8080/book', this.uploadItem, { headers: headerr, responseType: 'text' })
+    this.http.post('http://localhost:8080/api/book', this.uploadItem, { headers: headerr, responseType: 'text' })
       .subscribe({
 
         next: (data: any) => {
-          // Swal.fire({
-          //   position: 'center',
-          //   icon: 'success',
-          //   title: 'Post has been added',
-          //   showConfirmButton: false,
-          //   timer: 1500
-          // })
-          // this.router.navigateByUrl('home')
+         console.log(data)
         },
         error: (error: any) => {
           console.error(error);
