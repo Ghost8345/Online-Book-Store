35d3ed42cdc9783f69e62c75f3aaaa9ac42f0214
Aliaa Ibrahem <90799411+AliaaIbrahemAhmed@users.noreply.github.com>
2023-01-05 15:55:24 +0200

    handling notification requests

--- frontend/src/app/components/notifications/notifications.component.ts
+++ frontend/src/app/components/notifications/notifications.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {StockServiceService} from "../../stock-service.service";
+import {catchError, throwError} from "rxjs";
+import {MessageComponent} from "../message/message.component";
+import {StockOrder} from "../placeorder/StockOrder";
+
 
 @Component({
   selector: 'app-notifications',
@@ -6,5 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent {
-neworders=[[{name:"harrypoter",quantity:5}],[{name:"harrypoter",quantity:5},{name:"harrypoter",quantity:5}]]
+
+  constructor(@Inject(StockServiceService) private stockService: StockServiceService) {
+  };
+
+  stockOrders: StockOrder[] = [];
+  neworders = [[{name: "harrypoter", quantity: 5}], [{name: "harrypoter", quantity: 5}, {
+    name: "harrypoter",
+    quantity: 5
+  }]];
+
+  onConfirm(orderId: number): void {
+    let id = localStorage.getItem("user_id");
+    this.stockService.confirmOrder(Number(id), orderId);
+  }
+
+  getPendingOrders(): any {
+    let id = localStorage.getItem("user_id");
+    this.stockService.getPendingOrders(Number(id)).subscribe((response) => {
+        this.stockOrders = response;
+        for (let i = 0; i < response.length; i++) {
+            console.log(response[i].id, response[i].isbn,response[i].quantity);
+        }
+      }
+    );
+  }
 }
